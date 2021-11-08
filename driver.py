1d6241d8ff06b146c981aeb6b121c0d77c87352e
Max Hariri-Turner <maxkht8@gmail.com>
2021-11-07 23:03:41 -0800

    imported pyglet

--- driver.py
+++ driver.py
@@ -1,2 +1,35 @@
 #!/bin/bash
-# fuck you eat shit
+
+"""
+driver.py: does stuff
+"""
+
+__author__ = "Max Hariri-Turner"
+__email__ = "maxkht8@gmail.com"
+
+import pyglet
+from pyglet.window import key
+
+window = pyglet.window.Window()
+
+def main():
+    print("sugma")
+    pyglet.app.run()
+
+
+@window.event
+def on_draw():
+    window.clear()
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.A:
+        print('The "A" key was pressed.')
+    elif symbol == key.LEFT:
+        print('The left arrow key was pressed.')
+    elif symbol == key.ENTER:
+        print('The enter key was pressed.')
+
+if __name__ == "__main__":
+    main()
